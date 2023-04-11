083b2bd8533f0fd00aa87aa346f7c5de80a71598
Jacek Kowalski <JACEK.KOWALSKI1984@GMAIL.COM>
2023-04-11 10:25:13 +0200

    Fix: Add error handling
    Added error handling for the following scenarios:
    
    - No LEGO set exists with the given ID
    - Multiple search results match the same LEGO ID
    - A search result exists, but no price is listed for the given LEGO ID

--- LEGO.py
+++ LEGO.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
   
 
-LEGO_ID = "41916"
+LEGO_ID = "41903"
 
 
 def get_amazon_url (lego_id:str)->str:
@@ -14,19 +14,30 @@ def get_amazon_url (lego_id:str)->str:
 
 
 def define_title_with_lego_id (titles: list, lego_id: str):
+    titles_with_lego_id = []
     for t in titles:
         if lego_id in t.text:
-            title_with_lego_id = t
-            return title_with_lego_id
+            titles_with_lego_id.append (t)
+    if len(titles_with_lego_id) == 0:
+        raise ValueError ('No matches for this LEGO ID.')
+    elif len(titles_with_lego_id) > 1:
+        raise ValueError ("There are more then 1 matches for this LEGO ID")
+    else:
+        return titles_with_lego_id[0]
 
 
 def scrap_price_whole (url: str, lego_id: str)->str:
     driver = webdriver.Chrome()
     driver.get(url)
-    titles = driver.find_elements(By.XPATH, r'//*[@id="search"]/div[1]/div[1]/div/span[1]/div[1]/div/div/div/div/div/div[2]')
+    titles = driver.find_elements(By.XPATH, r'//*[@id="search"]/div[1]/div[1]/div/span[1]/div[1]/div/div/div/div/div/div')
     title_with_lego_id = define_title_with_lego_id (titles, lego_id)
+    print('\n', title_with_lego_id.text)
     price_element = title_with_lego_id.find_elements(By.XPATH, r'.//*[@class="a-price"]')
-    price_whole = price_element[0].text
+    if len(price_element) == 0:
+        driver.quit
+        raise ValueError ('There is no price listed for this LEGO ID.')
+    else:
+        price_whole = price_element[0].text
     driver.quit
     return price_whole
 
@@ -42,8 +53,11 @@ def string_to_float (text: str)->float:
 
 def main(lego_id:str)->None:
     url = get_amazon_url(lego_id)
-    price_whole = scrap_price_whole (url, lego_id)
-    amazon_current_price = string_to_float (price_whole) 
+    try:
+        price_whole = scrap_price_whole (url, lego_id)
+        amazon_current_price = string_to_float (price_whole)
+    except ValueError:
+        amazon_current_price = 'xxx'    
     print (amazon_current_price)
 
 
